c8c23faf66fde760b402c8341c15fff83250e275
IanaCris <ianasousati@gmail.com>
2021-05-13 23:34:57 -0300

    list posts

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,10 +1,24 @@
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
-import { getPrismicClient } from '../../services/prismic';
 import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
+
 import styles from './styles.module.scss';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostsProps {
+  posts: Post[]
+}
+
+export default function Posts({ posts }: PostsProps) {
   return (
     <>
       <Head>
@@ -13,26 +27,13 @@ export default function Posts() {
 
       <main className={styles.container}>
         <div className={styles.posts}>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong> Create a mongobd with Lerna</strong>
-            <p>In this guide, you will learn how to create a Monday</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong> Create a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how to create a monorepo</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong> Create a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how to create a monorepo</p>
-          </a>
-          <a href="#">
-            <time>12 de março de 2021</time>
-            <strong> Create a monorepo with Lerna</strong>
-            <p>In this guide, you will learn how to create a monorepo</p>
-          </a>
+          { posts.map(post => (
+            <a key={post.slug} href="#">
+              <time>{post.updatedAt}</time>
+              <strong>{post.title}</strong>
+              <p>{post.excerpt}</p>
+            </a>
+          )) }          
         </div>
       </main>
     </>
@@ -49,9 +50,24 @@ export const getStaticProps: GetStaticProps = async () => {
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2));
+  const posts = response.results.map(post => {
+    return {
+      slug: post.uid,
+      title: RichText.asText(post.data.title),
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    };
+  });
+
+  //console.log(JSON.stringify(response, null, 2));
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
